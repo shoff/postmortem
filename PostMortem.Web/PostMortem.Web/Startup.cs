91ee2ca73c415f24e50ec146a589ae9c106ad71e
Steve Hoff <smhoff256@gmail.com>
2019-07-10 21:39:29 -0500

    fixing secrets

--- PostMortem.Web/PostMortem.Web/Startup.cs
+++ PostMortem.Web/PostMortem.Web/Startup.cs
@@ -18,7 +18,8 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.InitializeContainer(this.Configuration)
+            services
+                .InitializeContainer(this.Configuration)
                 .InitializeOptions(this.Configuration)
                 .InitializeSwagger()
                 .AddCors()
