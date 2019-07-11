f34f4da5fdd2d2885de00a566f05993044d54feb
Steve Hoff <Steve.Hoff@identifix.com>
2019-07-11 11:58:45 -0500

    Fixing merge conflicts and adding in events.

--- PostMortem.Web/PostMortem.Infrastructure/Events/EventBroker.cs
+++ PostMortem.Web/PostMortem.Infrastructure/Events/EventBroker.cs
@@ -10,11 +10,22 @@
     {
         public event EventHandler<ProjectCreatedEventArgs> ProjectCreated;
         public event EventHandler<CommentDislikedEventArgs> CommentDisliked;
+        public event EventHandler<CommentLikedEventArgs> CommentLiked;
 
+
+        public void CreateProject(object sender, ProjectCreatedEventArgs eventArgs)
+        {
+            this.ProjectCreated.Raise(sender, eventArgs);
+        }
 
         public void DislikeComment(object sender, CommentDislikedEventArgs eventArgs)
         {
             this.CommentDisliked.Raise(sender, eventArgs);
+        }
+
+        public void LikeComment(object sender, CommentLikedEventArgs eventArgs)
+        {
+            this.CommentLiked.Raise(sender, eventArgs);
         }
     }
 }
